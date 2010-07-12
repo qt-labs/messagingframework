qt-labs/messagingframework

commit 9b6064522c01f9a9b83a0149cfd44e9dbd61d0e3

Fixes:    Retrieval of mail for non selectable IMAP folders.
Task:     NB#166648 kinda
Details:  Fix for 166648 introduced a regression that this patch fixes.

src/plugins/messageservices/imap/imapclient.cpp
@@ -720,6 +720,11 @@ void ImapClient::mailboxListed(const QString &flags, const QString &path)
             else
                 folder.setStatus(QMailFolder::ChildCreationPermitted, true);
 
+            if(flags.contains("\\NoSelect"))
+                folder.setStatus(QMailFolder::MessagesPermitted, false);
+            else
+                folder.setStatus(QMailFolder::MessagesPermitted, true);
+
             // The reported flags pertain to the listed folder only
             QString folderFlags;
             if (mailboxPath == path) {

src/libraries/qtopiamail/qmailfolder.cpp
@@ -57,6 +57,7 @@ static quint64 childCreationPermittedFlag = 0;
 static quint64 renamePermittedFlag = 0;
 static quint64 deletionPermittedFlag = 0;
 static quint64 nonMailFlag = 0;
+static quint64 messagesPermittedFlag = 0;
 
 class QMailFolderPrivate : public QSharedData
 {
@@ -144,6 +145,7 @@ public:
             renamePermittedFlag = registerFlag("RenamePermitted");
             deletionPermittedFlag = registerFlag("DeletionPermitted");
             nonMailFlag = registerFlag("NonMail");
+            messagesPermittedFlag = registerFlag("MessagesPermitted");
         }
     }
 
@@ -268,6 +270,52 @@ private:
 */
 
 /*!
+    \variable QMailFolder::ChildCreationPermitted
+
+    The status mask needed for testing the value of the registered status flag named 
+    \c "ChildCreationPermitted" against the result of QMailFolder::status().
+
+    This flag indicates that creation of child folders of this folder is allowed.
+*/
+
+/*!
+    \variable QMailFolder::RenamePermitted
+
+    The status mask needed for testing the value of the registered status flag named 
+    \c "RenamePermitted" against the result of QMailFolder::status().
+
+    This flag indicates that the folder may be renamed.
+*/
+
+/*!
+    \variable QMailFolder::DeletionPermitted
+
+    The status mask needed for testing the value of the registered status flag named 
+    \c "DeletionPermitted" against the result of QMailFolder::status().
+
+    This flag indicates that the folder may be deleted.
+*/
+
+/*!
+    \variable QMailFolder::MessagesPermitted
+
+    The status mask needed for testing the value of the registered status flag named 
+    \c "MessagesPermitted" against the result of QMailFolder::status().
+
+    This flag indicates that the folder is allowed to contain messages.
+*/
+
+/*!
+    \variable QMailFolder::NonMail
+
+    The status mask needed for testing the value of the registered status flag named 
+    \c "NonMail" against the result of QMailFolder::status().
+
+    This flag indicates that the folder contains only non mail items such as calendar 
+    and/or contacts items.
+*/
+
+/*!
     \enum QMailFolder::StandardFolder
 
     This enum type describes the standard folders pertinent to many account types.
@@ -295,6 +343,7 @@ const quint64 &QMailFolder::ChildCreationPermitted = childCreationPermittedFlag;
 const quint64 &QMailFolder::RenamePermitted = renamePermittedFlag;
 const quint64 &QMailFolder::DeletionPermitted = deletionPermittedFlag;
 const quint64 &QMailFolder::NonMail = nonMailFlag;
+const quint64 &QMailFolder::MessagesPermitted = messagesPermittedFlag;
 
 
 /*!

src/libraries/qtopiamail/qmailfolder.h
@@ -77,6 +77,7 @@ public:
     static const quint64 &RenamePermitted;
     static const quint64 &DeletionPermitted;
     static const quint64 &NonMail;
+    static const quint64 &MessagesPermitted;
 
     QMailFolder();
 

src/plugins/messageservices/imap/imapservice.cpp
@@ -199,10 +199,8 @@ bool ImapService::Source::retrieveMessageList(const QMailAccountId &accountId, c
     } else {
         // Retrieve messages for all folders in the account that have undiscovered messages
         QMailFolderKey accountKey(QMailFolderKey::parentAccountId(accountId));
-        QMailFolderKey undiscoveredKey(QMailFolderKey::serverUndiscoveredCount(0, QMailDataComparator::GreaterThan));
-        QMailFolderKey filterKey(accountKey);
-        if (minimum <= 1)
-            filterKey &= undiscoveredKey;
+        QMailFolderKey canSelectKey(QMailFolderKey::status(QMailFolder::MessagesPermitted));
+        QMailFolderKey filterKey(accountKey & canSelectKey);
         folderIds = QMailStore::instance()->queryFolders(filterKey, QMailFolderSortKey::id(Qt::AscendingOrder));
     }
 

commit a0acb0128b9980cd224d44bdb0d1a8c087a52477

Fixes:    Fix regression caused by 9b6064522c01f9a9b83a0149cfd44e9dbd61d0e3
Task:     Original task was NB#166648

Details:  Couldn't create subfolders in qtmail for IMAP folders because
          "\\NoInferiors" parsing logic was accidentally inverted.

src/plugins/messageservices/imap/imapclient.cpp
@@ -686,7 +686,7 @@ void ImapClient::mailboxListed(const QString &flags, const QString &path)
     QStringList list = _protocol.flatHierarchy() ? QStringList(path) : path.split(_protocol.delimiter());
 
     for ( QStringList::Iterator it = list.begin(); it != list.end(); ++it ) {
-        bool childCreationPermitted(flags.contains("\\NoInferiors", Qt::CaseInsensitive));
+        bool childCreationPermitted(!flags.contains("\\NoInferiors", Qt::CaseInsensitive));
         bool messagesPermitted(!flags.contains("\\NoSelect", Qt::CaseInsensitive));
         
         if (!mailboxPath.isEmpty())