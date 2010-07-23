qt-labs/messagingframework

commit 379a2e2fa5287dd3b492065caf9531ad5762b11e

When composing new message don't discard message currently being composed without user confirmation.

examples/qtmail/app/writemail.cpp
@@ -385,6 +385,18 @@ bool WriteMail::prepareComposer(QMailMessage::MessageType type, const QMailAccou
 {
     bool success = false;
 
+    // Don't discard mail being composed without user intervention
+    if (changed()) {
+        if (QMessageBox::question(qApp->activeWindow(),
+                                  tr("Compose new message"),
+                                  tr("A message is currently being composed. Do you wish to save the message in drafts?"),
+                                  QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes) {
+            draft();
+        } else {
+            return false;
+        }
+    }
+    
     reset();
 
     if (type == QMailMessage::AnyType) {

commit f687e806597f523fab9167573f389cbc66817cb1

Don't ask for confirmation to close message currently being composed when no message is being composed.
Regression introduced by 379a2e2fa5287dd3b492065caf9531ad5762b11e

examples/qtmail/app/writemail.cpp
@@ -385,7 +385,7 @@ bool WriteMail::prepareComposer(QMailMessage::MessageType type, const QMailAccou
 {
     bool success = false;
 
-    // Don't discard mail being composed without user intervention
+    // Don't discard mail being composed without user confirmation
     if (changed()) {
         if (QMessageBox::question(qApp->activeWindow(),
                                   tr("Compose new message"),
@@ -475,6 +475,13 @@ void WriteMail::statusChanged(const QString& status)
     else
         setWindowTitle(status);
 }
+
+void WriteMail::closeEvent(QCloseEvent *event)
+{
+    reset();
+    QMainWindow::closeEvent(event);
+}
+
 
 bool WriteMail::composerSelected(const QPair<QString, QMailMessage::MessageType> &selection)
 {

examples/qtmail/app/writemail.h
@@ -98,6 +98,9 @@ protected slots:
     bool composerSelected(const QPair<QString, QMailMessage::MessageType> &selection);
     void statusChanged(const QString& status);
 
+protected:
+    void closeEvent(QCloseEvent * event);
+
 private:
     bool largeAttachments();
     bool buildMail(bool includeSignature);