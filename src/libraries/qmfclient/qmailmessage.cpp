6c07a0fcd9afc89d17260d72d00e8b8b589785be
Don Sanders <don.sanders@nokia.com>
2011-02-21 15:48:38 +1000

    Integrate: http://meego.gitorious.org/meego-middleware/messagingframework/commit/3dbcc228504be4a3db74f07bbe4d918bec5284d4 Author: Javier Fernandez <jfernandez@igalia.com> Date:   Tue Feb 15 13:06:35 2011 +0100
        Fixes: NB#212210 - Utf8: Sender name is not shown correctly.

--- src/libraries/qmfclient/qmailmessage.cpp
+++ src/libraries/qmfclient/qmailmessage.cpp
@@ -6568,7 +6568,7 @@ void QMailMessageMetaData::setId(const QMailMessageId &id)
 QMailAddress QMailMessageMetaData::from() const
 {
     const QString& addr = impl(this)->_from;
-    const QString& decodedAddr = decodeWordSequence(addr.toUtf8());
+    const QString& decodedAddr = decodeWordSequence(addr.toAscii());
     return QMailAddress(decodedAddr);
 }
 
