017ae04cbc492d03300993e37f0936ab6cb73900
Don Sanders <don.sanders@nokia.com>
2012-02-24 13:46:49 +0200

    Ensure all Nokia source code has a valid and identical copyright notice.
    Mainly Symbian files were missing it.

--- src/libraries/qmfclient/qmailthreadlistmodel.h
+++ src/libraries/qmfclient/qmailthreadlistmodel.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
@@ -38,6 +38,7 @@
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
+
 #ifndef QMAILTHREADLISTMODEL_H
 #define QMAILTHREADLISTMODEL_H
 
