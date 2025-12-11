qt-labs/messagingframework

commit 9da97137adaef6ec6991bd5b227440f77a314210

Avoid some more qtopia references
Change-Id: I36c493538d5c8b167e9bddf1979916773fc5f895
Reviewed-by: Damien Caliste <dcaliste@free.fr>
Reviewed-by: Pekka Vuorela <pvuorela@iki.fi>

benchmarks/tst_messageserver/tst_messageserver.cpp
@@ -318,12 +318,6 @@ void tst_MessageServer::runInChildProcess(TestFunction fn)
         return;
     }
 
-    /* We get these messages from not using a QtopiaApplication */
-    for (int i = 0; i < 2; ++i) {
-        QTest::ignoreMessage(QtWarningMsg, "Object::connect: No such signal QApplication::appMessage(QString,QByteArray)");
-        QTest::ignoreMessage(QtWarningMsg, "Object::connect:  (sender name:   'tst_messageserver')");
-    }
-
     (this->*fn)();
 
     int exitcode = 0;

tests/tst_qmailmessageheader/tst_qmailmessageheader.cpp
@@ -1162,10 +1162,10 @@ void tst_QMailMessageHeaderField::output_data()
 
     QTest::newRow("Unbreakable token after breakable whitespace")
         << QByteArray("Content-Disposition")
-        << QByteArray("attachment; filename=/home/a_user_with_a_long_username/qtopia/home/Documents/channell1.jpg")
+        << QByteArray("attachment; filename=/home/a_user_with_a_long_username/qmf/home/Documents/channell1.jpg")
         << QByteArray(
 "Content-Disposition: attachment;" CRLF
-" filename=/home/a_user_with_a_long_username/qtopia/home/Documents/channell1.jpg" CRLF
+" filename=/home/a_user_with_a_long_username/qmf/home/Documents/channell1.jpg" CRLF
 " " CRLF);
 }
 

commit 45822c8f2b828d3d1ff2ebcab06f7a3c2b46a73a

Fix running tst_qmailmessageheader and one test inside it
Commit 9da97137adae broke the output test by slightly shorter string.
Went unnoticed as the whole test was marked passed no matter how many
failures it had.

Change-Id: I65d3321541d96bee0d7c56fd2fecaed84bb60b61
Reviewed-by: Pekka Vuorela <pvuorela@iki.fi>
Reviewed-by: <matti.viljanen@kapsi.fi>
Reviewed-by: Damien Caliste <dcaliste@free.fr>

benchmarks/tst_messageserver/tst_messageserver.cpp
@@ -769,11 +769,6 @@ void tst_MessageServer::onProgressChanged(uint value, uint total)
 
 int main(int argc, char** argv)
 {
-    /*
-        Ensure that this test doesn't run as the QWS server.
-        This is because we intend to fork() later on and we don't want to
-        have to process events for the sake of the child process.
-    */
     QCoreApplication app(argc, argv);
 
     int iters = 1;

tests/tst_qmailmessageheader/tst_qmailmessageheader.cpp
@@ -175,12 +175,13 @@ int main(int argc, char *argv[])
     tst_QMailMessageContentType contentType;
     tst_QMailMessageContentDisposition contentDisposition;
 
-    QTest::qExec(&dummy, argc, argv);
-    QTest::qExec(&messageHeader, argc, argv);
-    QTest::qExec(&contentType, argc, argv);
-    QTest::qExec(&contentDisposition, argc, argv);
+    int retVal = 0;
+    retVal += QTest::qExec(&dummy, argc, argv);
+    retVal += QTest::qExec(&messageHeader, argc, argv);
+    retVal += QTest::qExec(&contentType, argc, argv);
+    retVal += QTest::qExec(&contentDisposition, argc, argv);
 
-    return 0;
+    return retVal;
 }
 
 Q_DECLARE_METATYPE(QMailMessageHeaderField::ParameterType)
@@ -1162,10 +1163,10 @@ void tst_QMailMessageHeaderField::output_data()
 
     QTest::newRow("Unbreakable token after breakable whitespace")
         << QByteArray("Content-Disposition")
-        << QByteArray("attachment; filename=/home/a_user_with_a_long_username/qmf/home/Documents/channell1.jpg")
+        << QByteArray("attachment; filename=/home/a_user_with_a_long_username/qmf/home/Documents/extra/channell1.jpg")
         << QByteArray(
 "Content-Disposition: attachment;" CRLF
-" filename=/home/a_user_with_a_long_username/qmf/home/Documents/channell1.jpg" CRLF
+" filename=/home/a_user_with_a_long_username/qmf/home/Documents/extra/channell1.jpg" CRLF
 " " CRLF);
 }
 